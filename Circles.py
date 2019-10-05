4687225cebc70ba7dde5f0841532a179d6a5ad17
Jaren Glenn <jarenglenn@gmail.com>
2019-10-04 22:26:03 -0600

    Task 2, Assn 7 - Circles.py

--- Circles.py
+++ Circles.py
@@ -3,8 +3,6 @@ Jaren Glenn
 CS1400
 Assn 7 - 2
 
-See system analysis for description.
-
 Software Development Lifecycle
 
 System Analysis
@@ -16,32 +14,36 @@ System Design
 1. Ask user for the details of circle A
 2. Ask user for the details of circle B
 3. Convert input to integers
-4. Calculate the distance between the the centers using sqrt(|aX - bX| ** 2 + |aY - bY| ** 2)
-5. Compare the distance to the radius of the circles
+4. Evaluate the smaller circle
+5. Calculate the distance between the the centers using sqrt(|aX - bX| ** 2 + |aY - bY| ** 2)
+6. Compare the distance to the radius of the circles
    a. If distance <= rA - rB then the circles are inside each other
    b. If distance <= rA + rB then the circles are overlapping
    c. Else they are not touching
-6. Print the output of the comparison
+7. Print the output of the comparison
 
 Testing
 Test #1:
    Input:
       Circle A = (0.5, 5.1), radius = 13
-      Circle B = (1,, 1.7), radius = 4.5
+      Circle B = (1, 1.7), radius = 4.5
    Output:
       Circle B is inside Circle A
+   Passed
 Test #2:
    Input: 
       Circle A = (4.4, 5.7), radius = 5.5
       Circle B = (6.7, 3.5), radius = 3
    Output:
-      Circle B overlaps circle A
+      The circles are overlapping.
+   Failed - Had to fix some wrong operators in distance formula
 Test #3:
    Input:
       Circle A = (4.4, 5.5), radius = 1
       Circle B = (5.5, 7.2), radius = 1
     Outout:
-      Circle B does not overlap circle A  
+      The circles are not touching each other.
+   Passed
 '''
 from math import sqrt
 
@@ -49,15 +51,31 @@ from math import sqrt
 circleA = input("Please enter circle A's x,y coordinates and radius <x,y,r>: ").split(',')
 circleB = input("Please enter circle B's x,y coordinates and radius <x,y,r>: ").split(',')
 
-# Convert each number to an int
+# Convert each number to an int inside a list where
+# circle[0] = X Position
+# circle[1] = Y Position
+# circle[2] = Radius
+
 for num in circleA:
     circleA[circleA.index(num)] = float(num)
 
 for num in circleB:
     circleB[circleB.index(num)] = float(num) 
 
+# Evaluate the smaller circle
+if circleA[2] < circleB[2]:
+   smallerCircle, biggerCircle = 'A','B'
+else:
+   smallerCircle, biggerCircle = 'B','A'
 
 # Calculate distance between the centers of the circles
-distance = sqrt(4)
+distance = sqrt((abs(circleA[0] - circleB[0]) ** 2) + (abs(circleA[1] - circleB[1]) ** 2))
 
-print(distance)
+# Compare the distance to the radii and decide on output
+if distance <= abs(circleA[2] - circleB[2]):
+   print("Circle %s is inside circle %s." % (smallerCircle, biggerCircle))
+elif distance <= circleA[2] + circleB[2]:
+   print("The circles are overlapping.")
+else:
+   print("The circles are not touching each other.")
+   
