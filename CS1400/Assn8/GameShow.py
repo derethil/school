c78ea968c813526cc66798f107d62c537d968a9c
Jaren <Jaren Glenn>
2019-10-10 17:33:10 -0600

    GameShow.py

---
+++ CS1400/Assn8/GameShow.py
@@ -0,0 +1,12 @@
+'''
+Jaren Glenn
+CS1400
+Assn 8-2
+
+Software Development Lifecycle
+
+Requirement Specification
+    This program will simulate a game show 100,000 times. The game works as following:
+    Given three doors, one hides a car and the other two hide goats. The player chooses a door
+    and then one of the other doors
+'''
