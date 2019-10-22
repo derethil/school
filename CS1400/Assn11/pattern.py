0ab0642b5680c3b754f373ab2df787ef1248dc9f
Jaren <Jaren Glenn>
2019-10-21 21:12:51 -0600

    Started Assn11

---
+++ CS1400/Assn11/pattern.py
@@ -0,0 +1,37 @@
+'''
+Jaren Glenn
+CS1400 - 2
+Assn 11
+'''
+import turtle
+tr = turtle.Turtle()
+
+
+# Erase all patterns and start over
+def reset():
+    pass
+
+def setup():
+    pass
+
+# Draws rectangle pattern
+def drawRectanglePattern():
+    pass
+
+# Draw a single rectangle
+def drawRectangle():
+    pass
+
+# Draw circle pattern
+def drawCirclePattern():
+    pass
+
+# Draw super pattern
+def drawSuperPattern():
+    pass
+
+def setRandomColor():
+    pass
+
+def done():
+    pass
