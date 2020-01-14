96cdc8113ca31caeeaa7e4e69eddd27e28a3df12
Jaren Glenn <jarenglenn@gmail.com>
2020-01-14 16:01:33 -0700

    commit

--- CS1410/Assn1/src/ISBN.java
+++ CS1410/Assn1/src/ISBN.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
+/**
+ * @author Jaren Glenn
+ */
 public class ISBN {
-    public static void main(String[] args) {
-        System.out.print("Enter the first 9 digits of an ISBN: ");
+	/**
+	 * Calculates the 10th digit of a user-inputted 9-digit ISBN.
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.print("Enter the first 9 digits of an ISBN: ");
 
-        Scanner sc = new Scanner(System.in);
-        int start, input;
-        input = start = sc.nextInt();
+		// Get 9 ISBN input
+		Scanner sc = new Scanner(System.in);
+		final int INPUT = sc.nextInt();
+		int start = INPUT;
 
-        int digits[] = new int[9];
+		// Split the ISBN into an array for easy algebraic manipulation
+		int digits[] = new int[9];
 
-        for (int i = 8; i >= 0; i--) {
-            digits[i] = start % 10;
-            start /= 10;
-        }
+		for (int i = 8; i >= 0; i--) {
+			digits[i] = start % 10;
+			start /= 10;
+		}
 
-        int total = 0;
+		// Calculate the 10th digit
+		int total = 0;
+		for (int i = 0; i < 9; i++) {
+			total += digits[i] * (i + 1);
+		}
 
-        for (int i = 0; i < 9; i++) {
-            total += digits[i] * (i + 1);
-        }
-
-        System.out.println("The ISBN-10 number is: " + String.format("%09d", input) + (total % 11 != 10 ? total % 11 : "X"));
-    }
+		// Print out the final ISBN - and make sure the final digit is X in replacement of 10
+		System.out.println("The ISBN-10 number is: " + String.format("%09d", INPUT) + (total % 11 != 10 ? total % 11 : "X"));
+	}
 }
--- CS1410/Assn1/src/Quadratic.java
+++ CS1410/Assn1/src/Quadratic.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
+/**
+ * @author Jaren Glenn
+ */
 public class Quadratic {
+    /**
+     * Calculates the roots of a quadratic equation.
+     */
     public static void main(String[] args) {
         System.out.print("Enter a, b, c:");
 
         Scanner input = new Scanner(System.in);
 
+        // Sets each coefficient
         float numA = input.nextFloat();
         float numB = input.nextFloat();
         float numC = input.nextFloat();
 
+        // Calculates the roots and assigns the discriminant to a variable for later use
         double discriminant = Math.pow(numB, 2) - (4 * numA * numC);
-
         double r1 = (-numB + Math.sqrt(discriminant)) / (2 * numA);
         double r2 = (-numB - Math.sqrt(discriminant)) / (2 * numA);
 
+        // Checks how many roots there are and prints them accordingly
         if (discriminant > 0) {
             System.out.println("There are two roots for the quadratic equation with these coefficients.");
 
@@ -27,7 +35,7 @@ public class Quadratic {
             if (r1 == r2) {
                 System.out.println("r1 = " + r1);
 
-           } else if (r2 == Double.NaN) {
+           } else if (Double.isNaN(r2)) {
                 System.out.println("r1 = " + r1);
 
             } else {
