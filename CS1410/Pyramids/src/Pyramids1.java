70d5c92897fff47b1990052326456622eb716243
Jaren Glenn <jarenglenn@gmail.com>
2020-01-24 12:00:32 -0700

    Prints numbers for first pyramid

--- CS1410/Pyramids/src/Pyramids1.java
+++ CS1410/Pyramids/src/Pyramids1.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Pyramids1 {
+    public static void main(String[] args) {
+        System.out.print("Enter the number of lines: ");
+
+        Scanner userInput = new Scanner(System.in);
+
+        final int LINES = userInput.nextInt();
+        final int COL_LEN = String.valueOf(LINES).length() + 1;
+        final int TOTAL_LEN = (COL_LEN * LINES * 2) + 1;
+
+        for (int line = 1; line <= LINES; line ++) {
+
+            for (int num = line; num > 0; num--) {
+                System.out.printf("%" + COL_LEN + "d", num);
+            }
+
+            for (int num = 2; num <= line; num++) {
+                System.out.printf("%" + COL_LEN + "d", num);
+            }
+
+            System.out.print("\n");
+        }
+    }
 }
