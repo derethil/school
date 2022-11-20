4d5af3af45a6cb79ad2f40f2fb42c735d1c1aecd
Jaren Glenn <jarenglenn@gmail.com>
2022-11-20 16:17:16 -0700

    RequestHandler

--- cloudDevelopment/widget-request-handler/handler.py
+++ cloudDevelopment/widget-request-handler/handler.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+from validate import Validator
+from processor import Processor
+
+
+def widget_request_handler(event, context):
+    RequestHandler(event, context)
+
+
+class RequestHandler:
+    def __init__(self, event, context):
+        self.event = event
+        self.context = context
+
+        self.handle_request()
+
+    def handle_request(self):
+        request = Validator(self.event)
+
+        if request.validate():
+            processor = Processor("widgets")
+            processor.process(self.event)
