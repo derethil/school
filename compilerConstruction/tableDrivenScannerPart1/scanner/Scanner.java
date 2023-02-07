28aa2aa77a160825f2c63070871c0d273083a6ef
Jaren Glenn <jarenglenn@gmail.com>
2023-02-06 17:04:51 -0700

    removed todos

--- compilerConstruction/tableDrivenScannerPart1/scanner/Scanner.java
+++ compilerConstruction/tableDrivenScannerPart1/scanner/Scanner.java
@@ -7,32 +7,15 @@ import java.util.Stack;
  * This is the file you will modify.
  */
 public class Scanner {
-
-  //------------------------------------------------------------
-  // TODO: declare the HashMaps that you will use to store
-  // your tables. Also declare the start state.
-  //------------------------------------------------------------
-
     private final HashMap<Character, String> categoryTable = new HashMap<Character, String>();
     private final HashMap<String, HashMap<String, String>> transitionTable = new HashMap<String, HashMap<String, String>>();
     private final HashMap<String, String> tokenTypeTable = new HashMap<String, String>();
-
-
-  //------------------------------------------------------------
-  // TODO: build your tables in the constructor and implement
-  // the get methods.
-  //------------------------------------------------------------
 
   /**
    * Builds the tables needed for the scanner.
    */
   public Scanner(TableReader tableReader) {
-    // TODO: starting with the skeleton code below, build the
-    // classifer, transition and token type tables. You will need
-    // to also implement the test functions below once you have your
-    // tables built.
-
-    // Build catMap, mapping a character to a category.
+    // Build categoryTable, mapping a character to a category.
     for (TableReader.CharCat cat : tableReader.getClassifier()) {
       categoryTable.put(cat.getC(), cat.getCategory());
     }
@@ -55,9 +38,7 @@ public class Scanner {
   }
 
   /**
-   * Returns the category for c or "not in alphabet" if c has no category. Do not hardcode
-   * this. That is, this function should have nothing more than a table lookup
-   * or two. You should not have any character literals in here such as 'r' or '3'.
+   * Returns the category for c or "not in alphabet" if c has no category.
    */
   public String getCategory(Character c) {
     String category = this.categoryTable.get(c);
@@ -68,10 +49,7 @@ public class Scanner {
   }
 
   /**
-   * Returns the new state given a current state and category. This models
-   * the transition table. Returns "error" if there is no transition.
-   * Do not hardcode any state names or categories. You should have only
-   * table lookups here.
+   * Returns the new state given a current state and category.
    */
   public String getNewState(String state, String category) {
     HashMap<String, String> toStates = this.transitionTable.get(state);
@@ -94,22 +72,17 @@ public class Scanner {
     return tokenType;
   }
 
-  //------------------------------------------------------------
-  // TODO: implement nextToken
-  //------------------------------------------------------------
+  /**
+   * Returns true if the state is accepting.
+   */
+  private boolean isAccepting(String state) {
+    return this.tokenTypeTable.containsKey(state);
+  }
 
   /**
    * Return the next token or null if there's a lexical error.
    */
-
-  private boolean isAccepting(String state) {
-    return this.tokenTypeTable.containsKey(state);
-  }
   public Token nextToken(ScanStream ss) {
-    // TODO: get a single token. This is an implementation of the nextToken
-    // algorithm given in class. You may *not* use TableReader in this
-    // function. Return null if there is a lexical error.
-
     Stack<String> stack = new Stack<>();
     String state = "s0";
     StringBuilder lexeme = new StringBuilder();
