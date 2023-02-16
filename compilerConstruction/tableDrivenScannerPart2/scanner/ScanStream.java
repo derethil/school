75498a99f7354a174883632d146e83d840caefcc
Jaren Glenn <jarenglenn@gmail.com>
2023-02-15 17:37:12 -0700

    table driven scanner part 2

---
+++ compilerConstruction/tableDrivenScannerPart2/scanner/ScanStream.java
@@ -0,0 +1,40 @@
+/*
+ * DO NOT MODIFY THIS FILE
+ */
+package scanner;
+
+/**
+ * Abstracts a stream reader that reads one byte at a time with possible
+ * rollback.
+ *
+ * Do not modify this file.
+ */
+public final class ScanStream {
+
+  final private byte[] data;
+  private int i;
+
+  public ScanStream(byte[] data) {
+    this.data = data;
+    i = 0;
+  }
+
+  public boolean eof() {
+    return i >= data.length;
+  }
+
+  public char next() {
+    if (eof()) {
+      throw new RuntimeException("EOF reached");
+    }
+    return (char) data[i++];
+  }
+
+  public void printUpToNext() {
+    System.out.println(new String(data, 0, i-1));
+  }
+
+  void rollback() {
+    --i;
+  }
+}
