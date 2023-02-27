95063c9ce7a91904619bf253bde84717614d9456
Jaren Glenn <jarenglenn@gmail.com>
2023-02-26 18:53:13 -0700

    decision making midterm

--- decisionMaking/midterm/main.py
+++ decisionMaking/midterm/main.py
@@ -1,8 +1,10 @@
-import sys
-import matplotlib.pyplot as plt
+import math
+from abc import ABC, abstractmethod
 from argparse import ArgumentParser
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 """
 ===============================================================================
@@ -93,11 +95,21 @@ class Environment:
     """
 
     def __init__(self, baseline=False):
-        self.num_actions = 6 if baseline else 5
+        self.num_actions = 5
+        self.baseline = baseline
 
-    def reward(self, action, current_pull=0):
-        baseline, rewards = self.gen_rewards()
-        return rewards[action]
+    def reward(self, action):
+        rewards = self.gen_rewards()
+
+        reward = rewards[action]
+
+        if self.baseline:
+            baseline = np.max(np.random.normal(1.5, 3), 0)
+
+            if reward < baseline:
+                reward = 0
+
+        return reward
 
     def gen_rewards(self):
         rewards = np.array(
@@ -110,10 +122,7 @@ class Environment:
             ]
         )
 
-        if self.num_actions != len(rewards):
-            return np.random.normal(1.5, 3), rewards
-
-        return None, rewards
+        return rewards
 
 
 class Agent:
@@ -128,17 +137,23 @@ class Agent:
         self.Q = np.zeros(num_actions)
 
     def update_Q(self, action, reward):
+        if reward == 0:
+            return
+
         self.n[action] += 1
         self.Q[action] += (1.0 / self.n[action]) * (reward - self.Q[action])
 
-    def choose_action(self, environment, current_pull=None):
+    def choose_action(self, environment):
         if np.random.random() < self.eps:
             action = np.random.randint(self.num_actions)
         else:
-            action = np.random.choice(np.flatnonzero(self.Q == self.Q.max()))
+            action = np.argmax(self.Q)
 
-        reward = environment.reward(action, current_pull)
+        reward = environment.reward(action)
         return action, reward
+
+    def estimated_reward(self, action):
+        return self.Q[action]
 
     @property
     def estimated_best_reward(self):
@@ -150,59 +165,69 @@ class EpsilonGreedy:
     Run the epsilon greedy experiment
     """
 
-    def __init__(self, eps, num_experiments, num_pulls, drift=False, *args):
+    def __init__(self, eps, num_experiments, num_pulls, drift=False, baseline=False):
         self.eps = eps
         self.num_experiments = num_experiments
         self.num_pulls = num_pulls
         self.drift = drift
+        self.baseline = baseline
 
     def experiment(self):
         """
         Run a single experiment
         """
-        env = Environment()
+        env = Environment(self.baseline)
         agent = Agent(env.num_actions, self.eps)
         actions, rewards, estimated = [], [], []
 
-        for pull in range(self.num_pulls):
-            action, reward = agent.choose_action(env, pull if self.drift else None)
+        for _ in range(self.num_pulls):
+            action, reward = agent.choose_action(env)
             agent.update_Q(action, reward)
-            actions.append(action)
-            rewards.append(reward)
-            estimated.append(agent.estimated_best_reward)
 
-        return np.array(actions), np.array(rewards), np.array(estimated)
+            if reward != 0:
+                actions.append(action)
+                rewards.append(reward)
+                estimated.append(agent.estimated_best_reward)
 
-    def run(self, verbose=False):
+            else:
+                first = len(actions) == 0 or len(rewards) == 0 or len(estimated) == 0
+                estimated_from_action = agent.estimated_reward(action)
+                actions.append(actions[-1] if not first else action)
+                rewards.append(rewards[-1] if not first else reward)
+                estimated.append(estimated[-1] if not first else estimated_from_action)
+
+        return (
+            np.array(actions, dtype=float),
+            np.array(rewards, dtype=float),
+            np.array(estimated, dtype=float),
+            agent.Q,
+        )
+
+    def run(self):
         """
         Run the algorithm for the specified number of experiments
         """
-        if verbose:
-            print(f"Running Epsilon Greedy with epsilon={self.eps}...")
-
-        env = Environment()
-
+        env = Environment(self.baseline)
         R = np.zeros((self.num_pulls,))
         Q = np.zeros((self.num_pulls,))
-        A = np.zeros((self.num_pulls, env.num_actions))
+
+        Q_all = np.zeros(env.num_actions)
 
         for i in range(self.num_experiments):
-            actions, rewards, estimated = self.experiment()
-
-            fraction = self.num_experiments / 1
-            if verbose and ((i + 1) % (self.num_experiments / fraction) == 0):
-                eps_str = f"| eps = {self.eps:.2f} " if hasattr(self, "eps") else ""
-                print(
-                    f"[Experiment {i+1:02}/{self.num_experiments:2}] "
-                    + f"| avg_reward = {np.mean(rewards):.2f}"
-                )
+            _, rewards, estimated, agentQ = self.experiment()
 
             R += rewards
             Q += estimated
-            for j, a in enumerate(actions):
-                A[j, a] += 1
 
-        return R / self.num_experiments, Q / self.num_experiments
+            Q_all += np.array(agentQ)
+
+            report_progress(i, self.num_experiments)
+
+        return (
+            R / self.num_experiments,
+            Q / self.num_experiments,
+            Q_all / self.num_experiments,
+        )
 
 
 class Experiment:
@@ -211,14 +236,19 @@ class Experiment:
     """
 
     def __init__(
-        self, epsilons, num_experiments=1, num_pulls=365, verbose=False, drift=False
+        self,
+        epsilons,
+        num_experiments=1,
+        num_pulls=365,
+        drift=False,
+        baseline=False,
     ):
         self.epsilons = epsilons
 
         self.num_experiments = num_experiments
         self.num_pulls = num_pulls
-        self.verbose = verbose
         self.drift = drift
+        self.baseline = baseline
 
     def run(self, plot=False):
         """
@@ -226,14 +256,15 @@ class Experiment:
         """
         convergences = self.find_convergences()
 
-        for eps, (average, best) in zip(self.epsilons, convergences):
+        for eps, (average, best, actions) in zip(self.epsilons, convergences):
+            print(actions)
             print(
                 f"[Epsilon Greedy] "
                 + f"eps = {eps:.2f} | "
                 + f"experiments = {self.num_experiments} | "
                 + f"pulls = {self.num_pulls} | "
                 + f"avg_reward = {np.mean(average):.4f} | "
-                f"best_reward_estimate = {np.mean(best):.4f}"
+                f"best_reward_estimate = {np.mean(average):.4f}"
             )
 
             if plot:
@@ -250,9 +281,9 @@ class Experiment:
 
         for eps in self.epsilons:
             algorithm = EpsilonGreedy(
-                eps, self.num_experiments, self.num_pulls, self.drift
+                eps, self.num_experiments, self.num_pulls, self.drift, self.baseline
             )
-            result = algorithm.run(self.verbose)
+            result = algorithm.run()
             estimated_convergences.append(result)
 
         return estimated_convergences
@@ -270,26 +301,26 @@ class Experiment:
         plt.show()
 
 
-def run_epsilon(epsilons, plot=False):
+def run_epsilon(epsilons, plot=False, baseline=False):
     """
     Run the epsilon greedy algorithm with the given epsilons
     """
     print("Question 2: Moving Sensors")
-    experiment = Experiment(epsilons, num_experiments=1000)
+    experiment = Experiment(epsilons, num_experiments=100, baseline=baseline)
     experiment.run(plot)
     return experiment
 
 
-def actual_rewards(num=1000000, plot=False):
+def actual_rewards(num=1000000, plot=False, baseline=False):
     """
     Find the actual rewards by testing each sensor many, many times
     """
     print("Question 2: Moving Sensors")
-    env = Environment()
+    env = Environment(baseline=baseline)
 
     rewards = np.zeros(env.num_actions)
     for _ in range(num):
-        _, all_rewards = env.gen_rewards()
+        all_rewards = env.gen_rewards()
         rewards += all_rewards
 
     rewards /= num
@@ -311,6 +342,117 @@ def actual_rewards(num=1000000, plot=False):
 Question 3: American Options - Franchises
 ===============================================================================
 """
+
+
+class Contract(ABC):
+    def __init__(self, initial):
+        self.initial = initial
+
+    @abstractmethod
+    def get_payoff(self, paths):
+        raise NotImplementedError
+
+
+class EuropeanContract(Contract):
+    def get_payoff(self, paths):
+        return np.maximum(paths[:, -1] - self.initial, 0)
+
+
+class AmericanContract(Contract):
+    def __init__(self, initial, strike):
+        super().__init__(initial)
+        self.strike = strike
+
+    def get_payoff(self, paths):
+        first_greater_idx = np.argmax(paths > self.strike, axis=1)
+        first_greater = paths[np.arange(paths.shape[0]), first_greater_idx]
+        return np.maximum(first_greater - self.initial, 0)
+
+
+class Simulation:
+    def __init__(self, initial_price, drift_percent, volatility_percent, dt, T):
+        # Model Parameters
+        self.current_price = initial_price
+        self.initial_price = initial_price
+        self.drift_percent = drift_percent
+        self.volatility_percent = volatility_percent
+        # Time Parameters
+        self.dt = dt
+        self.T = T
+        # Price Path
+        self.prices = []
+
+    @property
+    def drift(self):
+        return self.current_price * self.drift_percent
+
+    def volatility(self, T, volatility_change=False):
+        if volatility_change and T < 0.5:
+            return self.current_price * 0.19
+
+        return self.current_price * self.volatility_percent
+
+    def simulate_path(self, volatility_change=False):
+        T = self.T
+        while T - self.dt > 0:
+            dWt = np.random.normal(0, math.sqrt(self.dt))
+            dYt = self.drift + self.volatility(T, volatility_change) * dWt
+            self.current_price += dYt
+            self.prices.append(self.current_price)
+            T -= self.dt
+
+        return self.prices
+
+
+class Model:
+    def __init__(self, num_paths, initial_price, drift, volatility, steps) -> None:
+        self.num_paths = num_paths
+        self.price_paths: list[int] = []
+
+        self.initial_price = initial_price
+        self.drift = drift
+        self.volatility = volatility
+        self.T = 1
+        self.dt = self.T / steps
+
+    def generate_paths(self, volatility_change=False):
+        for _ in range(self.num_paths):
+            sim = Simulation(
+                self.initial_price, self.drift, self.volatility, self.dt, self.T
+            )
+            price_path = sim.simulate_path(volatility_change=volatility_change)
+            self.price_paths.append(price_path)
+
+        return np.array(self.price_paths)
+
+
+def contract_value(num_paths, show_plot, contract_type, volatility_change):
+    print("Question 3: American Options - Franchises")
+    surge_str = "volatility change" if volatility_change else "no volatility change"
+    print(
+        f"Running {num_paths} simulations with {contract_type.title()} contract (with {surge_str})..."
+    )
+    initial_price = 385
+    model = Model(num_paths, initial_price, 0.04 / 12, 0.03, 120)
+    paths = model.generate_paths(volatility_change=volatility_change)
+
+    contract = (
+        EuropeanContract(initial_price)
+        if contract_type == "european"
+        else AmericanContract(initial_price, 535)
+    )
+    payoffs = contract.get_payoff(paths)
+
+    print(f"  Initial Value: {model.initial_price:.2f}")
+    print(f"  Average Final Value: {np.mean(paths[:, -1]):.2f}")
+    print(f"  Contract Value: {np.mean(payoffs):.2f}")
+
+    if show_plot:
+        for path in paths:
+            plt.plot(path)
+        plt.show()
+
+
 """
 ===============================================================================
 Main Program
@@ -349,15 +491,37 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--baseline",
+        help="Use the baseline sensor (question 2 only)",
+        action="store_true",
+    )
+
+    parser.add_argument(
         "--actual-rewards",
         help="Find the actual rewards (question 2 only)",
         action="store_true",
     )
 
+    # Question 3 arguments
+
+    parser.add_argument(
+        "--type",
+        help="Type of contract (question 3 only)",
+        choices=["european", "american"],
+        default="american",
+    )
+
+    parser.add_argument(
+        "--volatility-change",
+        help="Add a volatility surge to 19 at year 5 (question 3 only)",
+        action="store_true",
+    )
+
+    # Parse arguments
+
     args = parser.parse_args()
 
     match vars(args):
-
         # Question 1: Job Candidates
 
         case {"question": 1, "plot": should_plot, "rejection": rejection}:
@@ -375,16 +539,27 @@ if __name__ == "__main__":
             "plot": should_plot,
             "epsilons": epsilons,
             "actual_rewards": False,
+            "baseline": baseline,
         }:
-            run_epsilon(epsilons, plot=should_plot)
+            run_epsilon(epsilons, plot=should_plot, baseline=baseline)
 
-        case {"question": 2, "actual_rewards": True}:
-            actual_rewards()
+        case {
+            "question": 2,
+            "actual_rewards": True,
+            "plot": plot,
+            "baseline": baseline,
+        }:
+            actual_rewards(plot=plot, baseline=baseline)
 
         # Question 3: American Options - Franchises
 
-        case {"question": 3, "plot": should_plot, "rejection": _}:
-            print("Running Question 3")
+        case {
+            "question": 3,
+            "plot": should_plot,
+            "type": contract_type,
+            "volatility_change": volatility_change,
+        }:
+            contract_value(5000, should_plot, contract_type, volatility_change)
 
         # Invalid
         case _:
