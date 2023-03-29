c92e09ebb02dba77dcede041ded9310716b8befa
Jaren <jarenglenn@gmail.com>
2023-03-29 10:32:51 -0600

    imported mips homework

---
+++ compilerConstruction/hw-MIPS/code/add.asm
@@ -0,0 +1,8 @@
+	.data
+	
+	.text
+main:	
+	# Add code here
+
+	li	$v0,10
+	syscall
---
+++ compilerConstruction/hw-MIPS/code/fee.asm
@@ -0,0 +1,26 @@
+	.text
+
+	.globl	main
+main:
+        # put 3 and 4 on the stack using the register $sp
+
+        # call fee. You'll probably want to use the jal instruction to
+        # store the current address in $ra
+
+	# load results from stack to registers
+
+	# print result
+
+        # exit
+
+fee:
+        # copy a and b from stack to local registers
+
+	# add a and b
+
+        # place result on stack
+
+        # return using jr instruction
+	
+# Start .data segment (data!)
+	.data
